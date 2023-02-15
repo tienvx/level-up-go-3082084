b418d383099fa5fb8ef1e5b98f1b71560bd60b12
Tien Vo Xuan <tien.xuan.vo@gmail.com>
2023-02-15 15:40:35 +0000

    Implement 01_10

--- 01_10b/main.go
+++ 01_10b/main.go
@@ -2,39 +2,68 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"strconv"
 	"strings"
 )
 
 // operators is the map of legal operators and their functions
-var operators = map[string]func(x, y float64) float64{
-	"+": func(x, y float64) float64 { return x + y },
-	"-": func(x, y float64) float64 { return x - y },
-	"*": func(x, y float64) float64 { return x * y },
-	"/": func(x, y float64) float64 { return x / y },
+var operators = map[string]func(x, y float64) (*float64, error){
+	"+": func(x, y float64) (*float64, error) { val := x + y; return &val, nil },
+	"-": func(x, y float64) (*float64, error) { val := x - y; return &val, nil },
+	"*": func(x, y float64) (*float64, error) { val := x * y; return &val, nil },
+	"/": func(x, y float64) (*float64, error) {
+		if y == 0 {
+			return nil, fmt.Errorf("Can not divide by zero")
+		} else {
+			val := x / y
+			return &val, nil
+		}
+	},
 }
 
 // parseOperand parses a string to a float64
-func parseOperand(op string) float64 {
-	parsedOp, _ := strconv.ParseFloat(op, 64)
-	return parsedOp
+func parseOperand(op string) (*float64, error) {
+	parsedOp, error := strconv.ParseFloat(op, 64)
+	if error != nil {
+		return nil, fmt.Errorf("Can not parse number: %s", error)
+	}
+	return &parsedOp, nil
 }
 
 // calculate returns the result of a 2 operand mathematical expression
-func calculate(expr string) float64 {
+func calculate(expr string) (*float64, error) {
 	ops := strings.Fields(expr)
-	left := parseOperand(ops[0])
-	right := parseOperand(ops[2])
-	f := operators[ops[1]]
-	result := f(left, right)
-	return result
+	if len(ops) != 3 {
+		return nil, fmt.Errorf("Missing operand or operator")
+	}
+	left, error := parseOperand(ops[0])
+	if error != nil {
+		return nil, fmt.Errorf("can not parse left operand: %s", error)
+	}
+	right, error := parseOperand(ops[2])
+	if error != nil {
+		return nil, fmt.Errorf("can not parse right operand: %s", error)
+	}
+	f, ok := operators[ops[1]]
+	if !ok {
+		return nil, fmt.Errorf("Operator %s is not supported", ops[1])
+	}
+	result, error := f(*left, *right)
+	if error != nil {
+		return nil, fmt.Errorf("Can not evaluate expression %s: %s", expr, error)
+	}
+	return result, nil
 }
 
 func main() {
 	expr := flag.String("expr", "",
 		"The expression to calculate on, separated by spaces.")
 	flag.Parse()
-	result := calculate(*expr)
-	log.Printf("%s = %.2f\n", *expr, result)
+	result, error := calculate(*expr)
+	if error != nil {
+		log.Fatalf("Something wrong: %s\n", error)
+	}
+	log.Printf("%s = %.2f\n", *expr, *result)
 }
